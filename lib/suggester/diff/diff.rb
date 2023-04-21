a93997642a73639957a6bb7397c0001955013f4d
tick-taku <tick.taku.77@gamil.com>
2023-04-21 16:36:35 +0900

    Add property access

--- lib/suggester/diff/diff.rb
+++ lib/suggester/diff/diff.rb
@@ -1,8 +1,17 @@
 module GitHubDiff
   class Diff
+
     def initialize(file_path:, hunks:)
       @file_path = file_path
       @hunks = hunks
+    end
+
+    def file_path
+      return @file_path
+    end
+
+    def hunks
+      return @hunks
     end
   end
 end
--- lib/suggester/diff/hunk.rb
+++ lib/suggester/diff/hunk.rb
@@ -1,9 +1,22 @@
 module GitHubDiff
   class Hunk
+
     def initialize(code:, start_line:, end_line:)
       @code = code
       @start_line = start_line
       @end_line = end_line
+    end
+
+    def code
+      return @code
+    end
+
+    def start_line
+      return @start_line
+    end
+
+    def end_line
+      return @end_line
     end
   end
 end
